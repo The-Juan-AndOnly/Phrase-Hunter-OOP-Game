1269547040734c6fc7093fb7c8e1319e74f01a72
Juan Lopez <juanl0215@gmail.com>
2019-05-20 16:40:24 -0400

    Added Game Class

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,20 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.activePhrase = null;
+    this.phrases = [
+      { quote: 'May the Force be with you' },
+      { quote: 'I am going to make him an offer he can not refuse' },
+      { quote: 'Toto, I have got a feeling were not in Kansas anymore' },
+      { quote: 'Show me the money' },
+      {
+        quote:
+          'Life is like a box of chocolates You never know what you are gonna get'
+      }
+    ];
+  }
+}
