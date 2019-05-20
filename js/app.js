c1c097db53e56ac4eebe12faa116a28eea07a860
Juan Lopez <juanl0215@gmail.com>
2019-05-20 16:51:08 -0400

    Added app.js notes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+//clicking "Start Gane" creates new Game object
+
+// clicking on onscreen buton results to the handleInteraction() method
+
+// clicking on spaces between and around the onscreen
+// keyboard does not result in handleInteraction() being called
