dfb0e189f6ad58be4e5d225c86fdc9f9fe1d22be
Juan Lopez <juanl0215@gmail.com>
2019-05-20 16:16:54 -0400

    Added Phrase Class, constructor and methods

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,21 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase;
+  }
+
+  addPhraseToDisplay() {
+    //Add Phrase to gameboard
+  }
+
+  checkLetter() {
+    //Check if letter is in Phrase
+  }
+
+  showMatchedLetter() {
+    //Reveal letters that match player selection
+  }
+}
